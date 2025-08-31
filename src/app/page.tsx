d11b66169e8ed07a7031af5aceeb30b1c3a6dc2d
StepanAndrosov <stepan.news@yandex.ru>
2025-08-31 17:29:13 +0300

    feat: prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import { Button } from '@/shared/ui/button';
+import { prisma } from "@/shared/lib/db";
+import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
   return (
-    <div className='p-10'>
+    <div className="p-10 w-full flex flex-col gap-2">
+      {games.map((game) => (
+        <Card key={game.id} className="w-full max-w-[150px] p-2">
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
       <Button>Hello!</Button>
     </div>
   );
